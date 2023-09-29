9fd843ad608b5b25123e26acb7befd0edf78f86a
kishantalekar <kishantalekar024@gmail.com>
2023-09-29 22:44:42 +0530

    Day 9

---
+++ DAY_9/main.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n = 1;
+    // cout << n++;
+    // cout << n;
+    // cout << ++n;
+
+    // cout << n<<endl;
+
+    // cout << 13.6 / 'A'<<endl;
+    // cout << 'A' + 'A'<<endl;
+
+    //     cout << (5>4>3==0);
+
+    // if(2 && 3){
+    //     cout << "hello";
+    // }
+
+    // if(1 || 0){
+    //     cout << "or";
+    // }
+    // if(!0){
+    //     cout << endl
+    //          << "not";
+    // }
+
+    // cout << endl
+    //      << 'A'+0;
+
+    // char c;
+    // cout << "Enter a character :";
+    // cin >> c;
+    // if(c == 'a'||c=='e'|| c=='i'||c=='o'|| c=='u')
+    //     cout << "Vowels";
+    // else
+    //     cout << "consonants";
+
+    cout << (~1);
+    return 0;
+}
